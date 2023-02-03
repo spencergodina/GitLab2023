f60c1754742737d60ae9a3e815b19a426ca30b35
Spencer Godina <spencergodina@Spencers-MacBook-Pro-3.local>
2023-02-03 12:58:53 -0600

    commit arr

--- src/edu/unl/raikes/git/GitLab.java
+++ src/edu/unl/raikes/git/GitLab.java
@@ -2,7 +2,8 @@ package edu.unl.raikes.git;
 
 public class GitLab {
 	public static void main(String[] args) {
-		GitLabHelper helper = new GitLabHelper("Spencer", "Jay");
+		String[] persons = new String[] {"Spencer", "Jay"};
+		GitLabHelper helper = new GitLabHelper(persons);
 		helper.PrintWelcomeMessage();
 	}
 }
--- src/edu/unl/raikes/git/GitLabHelper.java
+++ src/edu/unl/raikes/git/GitLabHelper.java
@@ -1,17 +1,17 @@
 package edu.unl.raikes.git;
 
+import java.util.Arrays;
+
 public class GitLabHelper {
 	
-	private String person1;
-	private String person2;
+	private String[] persons;
 		
-	GitLabHelper(String person1, String person2) {
-		this.person1 = person1;
-		this.person2 = person2;
+	GitLabHelper(String[] persons) {
+		this.persons = persons;
 	}
 	
 	public void PrintWelcomeMessage() {
-		System.out.print("This is a lesson on git made by " + person1 + " and " + person2);
+		System.out.print("This is a lesson on git made by " + Arrays.toString(persons));
 	}
 
 }
