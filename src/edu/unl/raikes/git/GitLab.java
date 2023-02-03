94bf9bbebe0dc256a88d16d86788fae835632e6e
Spencer Godina <spencergodina@Spencers-MacBook-Pro-3.local>
2023-02-03 12:54:06 -0600

    commit

--- src/edu/unl/raikes/git/GitLab.java
+++ src/edu/unl/raikes/git/GitLab.java
@@ -2,6 +2,7 @@ package edu.unl.raikes.git;
 
 public class GitLab {
 	public static void main(String[] args) {
-		System.out.println("This is a lesson on git.");
+		GitLabHelper helper = new GitLabHelper("Spencer", "Jay");
+		helper.PrintWelcomeMessage();
 	}
 }
