810a456e7be5b6822ace9f3043714f6fee2041ce
Spencer Godina <spencergodina@Spencers-MacBook-Pro-3.local>
2023-02-03 12:34:31 -0600

    redo of initial commit

--- src/edu/unl/raikes/git/GitLab.java
+++ src/edu/unl/raikes/git/GitLab.java
@@ -2,6 +2,6 @@ package edu.unl.raikes.git;
 
 public class GitLab {
 	public static void main(String[] args) {
-		System.out.println("This is a lesson on git");
+		System.out.println("This is a lesson on git.");
 	}
 }
--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author spencergodina
- *
- */
-module GitLab2023 {
-}
